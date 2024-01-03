7acf0fee48f7b588b5656b6160652ef106ad5a6f
cbillard <cbillard@com-company.fr>
2024-01-03 11:22:27 +0100

    Project init and schemas creation for Sanity Backend

---
+++ schemas/workExperience.js
@@ -0,0 +1,21 @@
+export default {
+    name:'workExperience',
+    title:'Expériences professionnelles',
+    type:'document',
+    fields:[
+           {name:'name',
+               title:'name',
+               type:'string'
+            },
+            {
+                name:'company',
+                title:'Company',
+                type:'string'
+            },
+            {
+                name:'desc',
+                title:'Desc',
+                type:'string'
+            }
+    ]
+}
