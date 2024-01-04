6bc8b9b4c1b16284063e62bfd6fc1b057b291d2f
cbillard <cbillard@com-company.fr>
2024-01-04 10:30:50 +0100

    Adding the different section that constituate the portfolio

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 
+import { About, Footer, Header, Skills, Testimonials, Work } from './container'
+import { Navbar } from './components'
+
 function App() {
   return (
     <>
-      <h1>
-        App
-      </h1>
+      <div className='app'>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonials />
+        <Footer />
+      </div>
     </>
   )
 }
